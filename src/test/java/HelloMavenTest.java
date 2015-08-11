7264e3ae4753b03ccb0ea6d0465d3ff6b9d50ec0
unknown <00808911@WASJ45478.amtrak.ad.nrpc>
2015-08-11 10:16:36 -0400

    Initial Commit

---
+++ src/test/java/HelloMavenTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+
+public class HelloMavenTest extends FunctionalTestCase {
+	
+	@Test
+	public void sayHelloReturnsHelloMaven() throws Exception {
+		runFlowAndExpect("sayHello", "Hello Maven");
+	}
+	@Test
+	public void retrieveFlightsAddsAppropriate() throws Exception {
+		  // TODO Auto-generated method stub
+          MuleEvent event=runFlow("retrieveFlights");
+          String contentType=event.getMessage().getOutboundProperty("Content-Type");
+          assertEquals("application/json",contentType);
+	}
+	
+@Override
+protected String getConfigFile() {
+	// TODO Auto-generated method stub
+	return  "maven-project.xml";
+}
+}
